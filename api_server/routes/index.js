d157cfdc593f24a0b8416f0aaa697826d5688bfe
idlemike <idlemike@yandex.ru>
2020-10-18 14:16:40 +0300

    sel wo email

--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -31,7 +31,7 @@ router.post('/signup', createAccountLimiter, celebrate({
 // SIGNIN. selebrate, Joi
 router.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string(),
     password: Joi.string().required().min(8),
   }),
 }), login);
