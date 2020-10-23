c89463340e30f56c8c235cda6c4884b9885c10f9
idlemike <idlemike@yandex.ru>
2020-10-23 19:27:19 +0300

    links selebrate

--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -54,8 +54,8 @@ router.post('/articles', celebrate({
     text: Joi.string().required().min(2),
     source: Joi.string().required().min(2),
     date: Joi.date(),
-    link: Joi.string().required().pattern(new RegExp('^(https?|HTTPS?):\\/\\/(www.|WWW.)?((([a-zA-Z0-9-]{1,63}\\.){1,256}[a-zA-Z]{2,6})|((\\d{1,3}\\.){3}\\d{1,3}))(:\\d{2,5})?([-a-zA-Z0-9_,\\/.]{0,256}#?\\?)?$')),
-    image: Joi.string().required().pattern(new RegExp('^(https?|HTTPS?):\\/\\/(www.|WWW.)?((([a-zA-Z0-9-]{1,63}\\.){1,256}[a-zA-Z]{2,6})|((\\d{1,3}\\.){3}\\d{1,3}))(:\\d{2,5})?([-a-zA-Z0-9_,\\/.]{0,256}#?\\?)?$')),
+    link: Joi.string().required(),
+    image: Joi.string(),
   }),
 }), protect, postArticle);
 
